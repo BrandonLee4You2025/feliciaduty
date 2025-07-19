b0d990f258eadce7cfd2a2deb92719e9051929bc
BrandonLee4You2025 <brandon.lee@castlemill.run.place>
2025-07-19 15:31:59 -0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,23 +1,25 @@
+const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (req, res) => {
-  const proxy = createProxyMiddleware({
-    target: 'https://login.acceleratedmedicallinc.org',
-    changeOrigin: true,
-    onProxyReq: (proxyReq, req, res) => {
-      // Modify the request headers if needed
-      proxyReq.setHeader('Host', 'login.acceleratedmedicallinc.org');
-    },
-    onProxyRes: (proxyRes, req, res) => {
-      // Modify the response headers if needed
-    },
-  });
+  cors()(req, res, () => {
+    const proxy = createProxyMiddleware({
+      target: 'https://login.acceleratedmedicallinc.org',
+      changeOrigin: true,
+      onProxyReq: (proxyReq, req, res) => {
+        proxyReq.setHeader('Host', 'login.acceleratedmedicallinc.org');
+      },
+      onProxyRes: (proxyRes, req, res) => {
+        // Modify the response headers if needed
+      },
+    });
 
-  proxy(req, res, (err) => {
-    if (err) {
-      console.error('Proxy error:', err);
-      res.statusCode = 500;
-      res.end('Internal Server Error');
-    }
+    proxy(req, res, (err) => {
+      if (err) {
+        console.error('Proxy error:', err);
+        res.statusCode = 500;
+        res.end('Internal Server Error');
+      }
+    });
   });
 };
